65e909bc8fa94b9d05deef6afee5e5fc693f3f6b
Alexander Morton <mortonprod@gmail.com>
2017-06-18 00:07:34 +0100

    Add new buy page

---
+++ src/buy.js
@@ -0,0 +1,63 @@
+﻿import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom';
+import Vivus from 'vivus';
+import "./buy.css";
+
+import linesUnder from "./assets/linesUnderline.svg";
+import semi from "./assets/semiCircle.svg";
+
+import dress from './assets/tamara-bellis-256701.jpg'
+let isMounted = false;
+export default class Buy extends Component {
+    constructor(){
+        super();
+    }
+    componentDidMount(){
+        isMounted = true;
+    }
+    render() {
+        let  unter = null;
+        if(!isMounted){
+            unter = (
+                <object src={linesUnder} ref={() => {
+                    new Vivus("linesUnderline",{duration:"500",start:"autostart",file:linesUnder}); 
+                }} className="buy__underline" id="linesUnderline"></object>
+            )
+        }else{
+            unter = (
+                <img src={linesUnder} className={"buy__underline"} alt="chose"/>
+            )
+        }
+        return(
+        <div className={"buy"}>
+            <img src={this.props.img} className="buy__image" alt="logo" />
+            <div className={"buy__info"}>
+                <img src={semi} className="buy__info__image" alt="logo" />
+                <h2>
+                    {this.props.name}
+                </h2>
+                <p>
+                    {this.props.description}
+                </p>
+                <h3>
+                    {this.props.price}
+                </h3>
+                <button onClick = {()=>{}} className={"buy__button" }> </button>
+            </div>
+            {unter}
+        </div>
+        )
+    }
+}
+
+Buy.defaultProps = {
+    name:"Name Name Name",
+    description:"test description test test test test test test test test test test test test test test test test test test test test test test test test",
+    price:"£5 + delivery £2.5",
+    img:dress
+}
