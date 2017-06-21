34dd06d94c09e8b75a5d4b8d204baba413028374
Alexander Morton <mortonprod@gmail.com>
2017-06-21 22:59:50 +0100

    Add about page
    Create tranlate and scale dependent scroll

---
+++ src/about.js
@@ -0,0 +1,140 @@
+﻿import React, { Component } from 'react';
+import Vivus from 'vivus';
+import aboutUs from './assets/aboutUs.svg';
+import people from './assets/people.svg';
+import present from './assets/present.svg';
+import * as _ from "lodash";
+import "./about.css";
+let isMounted = false;
+export default class About extends Component {
+    constructor(){
+        super();
+        this.scroll = _.throttle(this.scroll.bind(this),this.time/this.steps);
+        this.state = {scale:[0,0],translateY:[0,0],top:[0,0]}
+    }
+    el = [];
+    scale = [0,0];
+    translateY = [0,0];
+    heightChunk = 0;
+    scaleChunk = 0;
+    scrollTop = null;
+    componentWillMount(){
+
+    }
+    componentDidMount(){
+        isMounted = true;
+        let top = [];
+        top[0] =  window.innerHeight;
+        top[1] =  window.innerHeight;
+        this.setState({top:top});
+        this.heightChunk = window.innerHeight/this.props.steps; 
+        this.scaleChunk = 1/this.props.steps; 
+        window.addEventListener('scroll', this.scroll.bind(this));
+    }
+    scroll(event){
+        let isMovingDown = null;
+        let scrollTop = event.srcElement.body.scrollTop;
+        if(event.srcElement.body.scrollTop <     this.scrollTop){
+           isMovingDown = true; 
+        }else{
+           isMovingDown = false;
+        }
+        let scale = [this.state.scale[0],this.state.scale[1]];
+        let translateY = [this.state.translateY[0],this.state.translateY[1]];
+        let top = [this.state.top[0],this.state.top[1]];
+        if(this.scrollTop !== null){
+            let moveNum = 1;
+            let height = window.innerHeight;
+            for(let i=0; i < moveNum; i++){
+                this.setState({scale: scale,top:top});
+                let bottom = this.el[i].getBoundingClientRect().bottom;
+	            let isAdd = null;
+	            if(bottom > height){///If object is at top half of screen then shrink agains 
+	                isAdd = true; 
+	            }else{
+	                isAdd = false;
+
+	            }
+	            if(isMovingDown){//If direction of scroll changes then skrink rather than expand or vis vera.
+	                if(isAdd){
+	                    scale[i] = this.scale[i] + this.scaleChunk;
+	                }else{
+	                    scale[i] = this.scale[i] - this.scaleChunk;
+	                }
+	            }else{
+	                if(!isAdd){
+	                    scale[i] = this.scale[i] + this.scaleChunk;
+	                }else{
+	                    scale[i] = this.scale[i] - this.scaleChunk;
+	                }
+                }
+	            if(isMovingDown){//Change direction of translate if scroll direction changes.
+	             //   translateY[0] = this.state.translateY + this.heightChunk;
+	             //   translateY[1] = this.state.translateY + this.heightChunk;
+	                  top[i] = this.state.top[i] + this.heightChunk;
+	            }else{
+	             //   translateY[0] = this.state.translateY - this.heightChunk;
+	              //  translateY[1] = this.state.translateY - this.heightChunk;
+	              top[i] = this.state.top[i] - this.heightChunk;
+	            }
+            }
+        }
+        this.setState({scale:scale, translateY:translateY, top:top});
+        this.scrollTop = scrollTop;
+    }
+    render()
+    {
+        let  title = null;
+        if(!isMounted){
+            title = (
+                <object ref={() => { 
+                    new Vivus("info",{duration:"2000",start:"autostart",file:aboutUs});                   
+                }} className="about__title" id="info"></object>
+            )
+        }else{
+            title = (
+                <img src={aboutUs} className={"about__title"} alt="about"
+                />
+            )
+        }
+        return (
+            <section className={"about"}>
+                {title}
+                <article
+                ref={(el)=>{this.el[0]= el}} 
+		        style={{
+                    transform:'scale(' + this.state.scale[0] + ') '  +  ' translateY(' + this.state.translateY[0] + ')',
+                    top:this.state.top[0]
+
+		         }}
+		         className={"about__entry"}>
+		                <img src={people} className={"about__entry__pic"} alt="about"/>
+		                <div className={"about__entry__info"} >
+		                    <h2>Always ready to help</h2>
+		                    <p>mortonprod@gmail.com  </p>
+		                    <p>0141 577 2197</p>
+		                </div>
+	            </article>
+	            <article
+                    ref={(el)=>{this.el[1]= el}} 
+	                style={{
+	                    transform:'scale(' + this.state.scale[1] + ') '  +  ' translateY(' + this.state.translateY[1] + ')',
+                        top:this.state.top[1]
+	                 }} 
+	                className={"about__entry"}>
+	                <img src={present} className={"about__entry__pic"} alt="about"/>
+	                <div className={"about__entry__info"}>
+	                    <h2>Discreet delivery and purchase</h2>
+	                    <p>No mention of product in bank statements </p>
+	                    <p>Inconspicuous packaging</p>
+	                </div>
+                </article>
+            </section>
+        );
+    }
+}
+
+About.defaultProps = {
+    time:3000,
+    steps:100
+}
