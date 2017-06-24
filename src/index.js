8d9337e1a334079fc311ffbdaea5143a534847cd
Alexander Morton <mortonprod@gmail.com>
2017-06-24 13:28:53 +0100

    Add test and logo and index

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
+import App from './App';
+import './index.css';
+
+//ReactDOM.render(
+//  <App />,
+ // document.getElementById('root')
+//);
+
+
+render(
+  <App />,
+  document.getElementById('root')
+);
